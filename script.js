da5877bf7423dd84f7300c7ede89f07d39d4f957
Sujoy Ghosh <103837998+sujoypuja@users.noreply.github.com>
2023-06-29 14:38:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo')
+})
